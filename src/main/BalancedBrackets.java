6cb268945de6688b403f5287d5d3b890b233c8bf
Kieran <kieranbarry51@gmail.com>
2022-04-27 19:44:37 -0500

    added unit tests and ajusted the code to pass them

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
 
 public class BalancedBrackets {
     /**
@@ -22,14 +26,58 @@ public class BalancedBrackets {
      * @return true if balanced, false otherwise
      */
     public static boolean hasBalancedBrackets(String str) {
-        int brackets = 0;
+
+        ArrayList<Integer>  binaryBracketCounter = new ArrayList<>();
+        if (!str.contains("[") || !str.contains("]")) {
+            return false;
+        }
+
         for (char ch : str.toCharArray()) {
             if (ch == '[') {
-                brackets++;
+                binaryBracketCounter.add(0);
             } else if (ch == ']') {
-                brackets--;
+                binaryBracketCounter.add(1);
             }
         }
-        return brackets == 0;
+
+        for (int i = 0; i < binaryBracketCounter.size()-1; i++){
+            if (binaryBracketCounter.get(i) + binaryBracketCounter.get(i+1) != 1){
+                return false;
+            } else if (binaryBracketCounter.get(0) != 0){
+                return false;
+            }
+        }
+
+        return true;
     }
+
 }
+
+
+
+// alternative method that didn't quite work
+//        int counter = -1;
+//        ArrayList<Integer> leftBrackets = new ArrayList<>();
+//        ArrayList<Integer> rightBrackets = new ArrayList<>();
+
+//        for (char ch : str.toCharArray()) {
+//            counter = counter + 1;
+//            if (ch == '[') {
+//                leftBrackets.add(counter);
+//            } else if (ch == ']') {
+//                rightBrackets.add(counter);
+//            }
+//        }
+//
+//        if (leftBrackets.size() != rightBrackets.size()){
+//            return false;
+//        }
+//
+//        for (int i = 0; i < leftBrackets.size()-1; i++){
+//            int testInt = i + 1;
+//            if (rightBrackets.get(i) < leftBrackets.get(i) || rightBrackets.get(testInt) < leftBrackets.get(testInt)){
+//                return false;
+//            } else if (rightBrackets.get(i) > leftBrackets.get(testInt)){
+//                return false;
+//            }
+//        }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +12,58 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void backwardsBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void backwardsBracketsWithTextReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]some text["));
+    }
+
+    @Test
+    public void nestedBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[this[is a]test]"));
+    }
+
+    @Test
+    public void singleBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void emptyStringReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void bracketsWithTextInBetweenReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[some text]"));
+    }
+
+    @Test
+    public void textContainingBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("some[text]"));
+    }
+
+    @Test
+    public void textWithNoBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("some text"));
+    }
+
+    @Test
+    public void multipleBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]watcha[know]about[]al[l]thatjazz[]"));
+    }
+
+
 
 
 }
